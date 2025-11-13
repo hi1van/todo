a67f9e6f1384fffe9e3887f3e48d9b319e6609eb
Ivan Zhuang <iczhuang@student.unimelb.edu.au>
2025-11-13 16:54:29 +1100

    fixed type bug

--- src/main/kotlin/com/example/todo/dto/TaskRequest.kt
+++ src/main/kotlin/com/example/todo/dto/TaskRequest.kt
@@ -1,11 +1,11 @@
 package com.example.todo.dto
 
 import com.example.todo.model.TaskStatus
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 class TaskRequest (
     val title: String,
     val description: String,
     val status: TaskStatus = TaskStatus.TODO,
-    val dueDate: LocalDateTime? = null
+    val dueDate: LocalDate? = null
 ){}
--- src/main/kotlin/com/example/todo/dto/TaskResponse.kt
+++ src/main/kotlin/com/example/todo/dto/TaskResponse.kt
@@ -1,12 +1,12 @@
 package com.example.todo.dto
 
 import com.example.todo.model.TaskStatus
-import java.time.LocalDateTime
+import java.time.LocalDate
 
 class TaskResponse (
     val id: String,
     val title: String,
     val description: String,
     val status: TaskStatus = TaskStatus.TODO,
-    val dueDate: LocalDateTime? = null
+    val dueDate: LocalDate? = null
 ) {}
